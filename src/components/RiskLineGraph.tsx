0f4104383cf4af804dea86a7f67ad2d24bf7309a
tushan-shahid <shahidshabraj@gmail.com>
2023-05-02 01:02:37 -0700

    line graph

--- src/components/RiskLineGraph.tsx
+++ src/components/RiskLineGraph.tsx
@@ -34,7 +34,7 @@ const options: ChartOptions<"line"> = {
     },
     title: {
       display: true,
-      text: "Risk Ratings by Year",
+      text: "Risk Ratings by Year and Business Category",
     },
   },
   scales: {
@@ -55,20 +55,43 @@ const options: ChartOptions<"line"> = {
 };
 
 const RiskLineGraph: React.FC<ChartProps> = ({ data }) => {
-  const year = data.map((d) => d.Year);
-  const riskRatings = data.map((d) => d["Risk Rating"]);
+  // Group data by business category
+  const categoriesData = data.reduce<Record<string, CsvData[]>>((acc, row) => {
+    if (!acc[row["Business Category"]]) {
+      acc[row["Business Category"]] = [];
+    }
+    acc[row["Business Category"]].push(row);
+    return acc;
+  }, {});
+
+  // Get all the years from the data
+  const yearsSet = new Set(data.map((d) => d.Year));
+  const years = Array.from(yearsSet).sort((a, b) => a - b);
+
+  // Create a dataset for each business category
+  const datasets = Object.keys(categoriesData).map((category) => {
+    const categoryData = categoriesData[category];
+
+    const categoryYears = categoryData.map((d: { Year: any }) => d.Year);
+    const riskRatings = categoryData.map(
+      (d: { [x: string]: any }) => d["Risk Rating"]
+    );
+
+    return {
+      label: category,
+      data: years.map((year) => {
+        const index = categoryYears.indexOf(year);
+        return index !== -1 ? riskRatings[index] : null;
+      }),
+      fill: false,
+      borderColor: `hsl(${Math.random() * 360}, 75%, 50%)`,
+      tension: 0.4,
+    };
+  });
 
   const chartData = {
-    labels: year,
-    datasets: [
-      {
-        label: "Risk Rating",
-        data: riskRatings,
-        fill: false,
-        borderColor: "rgb(75, 192, 192)",
-        tension: 0.4,
-      },
-    ],
+    labels: years,
+    datasets,
   };
 
   return <Line options={options} data={chartData} />;
